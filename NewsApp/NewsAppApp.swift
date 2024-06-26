671e9b7034fe62e179354836fdf91655f0d62647
Ahmad Asroruddin <asroruddinahmad09@gmail.com>
2024-06-27 06:03:18 +0700

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ahmad Asroruddin on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
